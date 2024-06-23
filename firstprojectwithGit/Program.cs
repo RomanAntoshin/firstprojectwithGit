0745b6add52a7ae07b53d86bffd4800a0ebf0636
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-06-23 20:35:20 +0300

    Увеличил число повторений

--- firstprojectwithGit/Program.cs
+++ firstprojectwithGit/Program.cs
@@ -6,7 +6,7 @@ namespace firstprojectwithGit
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Hello World!");
             }
