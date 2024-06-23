6ceb7c4ae398c166fcb9b2e5213bc7e01630a1c8
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-06-23 19:52:10 +0300

    Добавьте файлы проекта.

---
+++ firstprojectwithGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstprojectwithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
