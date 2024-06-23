f3f708e7a7e63d230e6ef3de2c43eb320d6135ba
Antoshin_Roman <rom.antoshin2016@yandex.ru>
2024-06-23 21:07:40 +0300

    Изменил выводимый текст

--- firstprojectwithGit/Program.cs
+++ firstprojectwithGit/Program.cs
@@ -8,7 +8,7 @@ namespace firstprojectwithGit
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Hello World!");
+                Console.WriteLine("Привет, Мир");
             }
         }
     }
